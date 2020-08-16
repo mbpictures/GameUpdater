4f17244df6a96d540aaadd4c92f6f0542c16ea4c
Marius Butz <mbpictures13@gmail.com>
2020-08-16 19:45:29 +0200

    add new project with tool to speed up server manifest creation

---
+++ ManifestTool/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+namespace ManifestTool
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
