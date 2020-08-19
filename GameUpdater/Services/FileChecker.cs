3a6b3a1bb06a9d3bb9c6002c926633ba68acc1ec
Marius Butz <mbpictures13@gmail.com>
2020-08-19 22:53:19 +0200

    add zip file checking

--- GameUpdater/Services/FileChecker.cs
+++ GameUpdater/Services/FileChecker.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Security.Cryptography;
+using System.Xml;
 using GameUpdater.Services.Download;
 
 namespace GameUpdater.Services
@@ -46,8 +48,46 @@ namespace GameUpdater.Services
                     temp.Remove(file.FileName);
                 }
             }
+            
+            _checkChecksumFiles(temp);
 
             return temp.Values.ToList();
+        }
+
+        private static void _checkChecksumFiles(Dictionary<string, DownloadFile> dict)
+        {
+            var checkFiles = Directory.GetFiles(
+                IniLoader.Instance.Read("GameDirectory", "General"), 
+                "*.checksum.xml",
+                SearchOption.AllDirectories);
+            checkFiles.ToList().ForEach(checkFile =>
+            {
+                var doc = new XmlDocument();
+                doc.Load(checkFile);
+                var zipFilename = doc.SelectSingleNode("checksums/zipFilename").InnerText;
+                var url = doc.SelectSingleNode("checksums/url")?.InnerText;
+                foreach (var file in doc.GetElementsByTagName("file").Cast<XmlNode>().ToList())
+                {
+                    var filename = Path.Combine(Path.GetDirectoryName(checkFile) ?? "./", file.SelectSingleNode("filename").InnerText);
+                    var md5 = file.SelectSingleNode("md5")?.InnerText;
+                    var sha1 = file.SelectSingleNode("sha1")?.InnerText;
+
+                    var fileValid = true;
+
+                    if (!string.IsNullOrEmpty(md5)) 
+                        fileValid &= _verifyMd5(filename, md5);
+                    if (!string.IsNullOrEmpty(sha1)) 
+                        fileValid &= _verifySha1(filename, sha1);
+
+                    if (!fileValid)
+                    {
+                        dict.Add(filename, new DownloadFile(url, zipFilename, md5, sha1, true));
+                        continue;
+                    }
+
+                    dict.Remove(filename);
+                }
+            });
         }
 
         private static bool _verifyMd5(string filename, string expectedMd5)
